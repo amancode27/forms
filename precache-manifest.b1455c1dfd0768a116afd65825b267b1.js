9cf7d8976313530d47b5e0eae66e1c0cf46d8c2e
amancode27 <43828332+amancode27@users.noreply.github.com>
2019-08-17 16:20:06 +0530

    Updates

---
+++ precache-manifest.b1455c1dfd0768a116afd65825b267b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9522546946c1dd98cfcce50e6a2929b7",
+    "url": "/forms/index.html"
+  },
+  {
+    "revision": "a5b0994f1eb141dc7a03",
+    "url": "/forms/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "98cf59efec316978a41e",
+    "url": "/forms/static/css/main.5c433d8f.chunk.css"
+  },
+  {
+    "revision": "a5b0994f1eb141dc7a03",
+    "url": "/forms/static/js/2.840e4f0f.chunk.js"
+  },
+  {
+    "revision": "98cf59efec316978a41e",
+    "url": "/forms/static/js/main.c8ff4eb1.chunk.js"
+  },
+  {
+    "revision": "7b433bcb0485557f4f92",
+    "url": "/forms/static/js/runtime~main.93bf94d6.js"
+  }
+]);
